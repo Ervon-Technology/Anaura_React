65d4b04b3d1bf295cf0790ebb96c894be60a87fd
Sambit  Pattanaik <pattanaiksambit80@gmail.com>
2024-09-14 18:04:23 +0530

    about us

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -6,6 +6,10 @@ import SectionTwo from '../Component/SectionTwo/SectionTwo';
 import AboutUsSection from '../Component/AboutUsSection/AboutUsSection';
 import CraftingSection from '../Component/CraftingSection/CraftingSection';
 import SavePlanet from '../Component/SavePlanet/SavePlanet';
+import gallery1 from '../../images/about/gallery-1.jpg'
+import gallery2 from '../../images/about/gallery-2.jpg'
+import gallery3 from '../../images/about/gallery-3.jpg'
+import gallery4 from '../../images/about/gallery-4.jpg'
 
 import BgHeroic from '../../images/about/about-heroic.png'
 
@@ -27,14 +31,17 @@ const About = () => {
 
                 <section>
                     <div class="row p-0 w-100">
-                        <div class="col-6 p-0">
-                            <img src="https://s3-alpha-sig.figma.com/img/8e86/2ef4/b04aa145fdb0d21bae205dd2ebd8f789?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JF11L1qDRIemkEXRMYxy~tD-EiepLZRr6RGYN8kkj~4gTiuFBWO23TMRcjLx4ZndKToLlqb-0zytvLHMcLkOXVsYJj8xnStQ3YG4nbz8cCWWpsi71GyYQF3EADoga-BdbAm4~dj1vefhA1xFW~9YV01do3K5-fJJ77YODlg0X0DPpV0~TgrORY7BQF6raTUFRmfnZ~nHeA70Mp~DyHg-aRyxio6fqNFXSQvP16PK0rYGB3E10-fER0agdy1NvFuubRQFIKRov619wopLyIMfQIMKoKjoNlzB1COchurfiNHPDolaacw-JUDnuTEmwA0W1coyVIPijpvE54sma9PvMw__" alt="" class="w-100 h-100 object-fit-cover img-fluid" />
+                        <div class="col-3 p-0">
+                            <img src={gallery1} alt="" class="w-100 h-100 object-fit-cover img-fluid" />
                         </div>
                         <div class="col-3 p-0">
-                            <img src={Brand} alt="" class="w-100 h-100 object-fit-cover img-fluid" />
+                            <img src={gallery2} alt="" class="w-100 h-100 object-fit-cover img-fluid" />
                         </div>
                         <div class="col-3 p-0">
-                            <img src="https://s3-alpha-sig.figma.com/img/cae2/ecf5/ed561df87b05573f77a6eb0076312aad?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Fu4tGxVN6~0JSHSs9QLBKR-GuZOj1O3SFjtY07h-oICKfXYENiiYrWT6BSVXAzsiIcNv8cNuWA8RR3TQ657J4BIiUYZFR2gCYKBdWHvBy6roNWnmAR7owJwxOZ1L2vwfZVxtaiN6CPpa2k5D1yc7JNLCzAa5KaqkgiVchISY2-Xq3J2qFBisJ-DxumppbZpaPCQYZTIz2cQOqDOCHSbEFHQTTjc2nvhYyFYamhrJJsbK08ur4P4kiTvkVsy9kHm-GfnjU1JwiTlMlHy8zhCuSM8vSb4pQFJAjds~ha1PCzEfCEmRkVqywXx6mqES-bgoCJlXgbjDVTOVmkWXwAIBaQ__" alt="" class="w-100 h-100 object-fit-cover img-fluid" />
+                            <img src={gallery3} alt="" class="w-100 h-100 object-fit-cover img-fluid" />
+                        </div>
+                        <div class="col-3 p-0">
+                            <img src={gallery4} alt="" class="w-100 h-100 object-fit-cover img-fluid" />
                         </div>
                     </div>
                 </section>
--- src/Pages/Component/AboutUsSection/AboutUsSection.js
+++ src/Pages/Component/AboutUsSection/AboutUsSection.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AboutUsSection = () => {
+  const [activeTab, setActiveTab] = useState("anaura");
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Detect screen size to switch between mobile/tablet and desktop layout
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <section className="py-5">
       <div className="container py-5">
@@ -8,109 +25,108 @@ const AboutUsSection = () => {
           <div className="col-lg-4 col-md-12">
             <p className="mb-4 fs-1">About Us</p>
 
-            <div
-              className="nav flex-column nav-pills me-3"
-              id="v-pills-tab"
-              role="tablist"
-              aria-orientation="vertical"
-            >
+            {/* Navigation - Tabs */}
+            <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
               <div
-                className="fs-4 my-3 pb-3 ps-0"
-                style={{ borderBottom: '2px dotted rgb(105, 105, 105)' }}
+                className={`fs-4 my-3 pb-3 ps-0 ${activeTab === "anaura" ? 'text-primary' : ''}`}
+                style={{ borderBottom: '2px dotted rgb(105, 105, 105)', cursor: "pointer" }}
                 id="v-pills-home-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-home"
-                type="div"
-                role="tab"
-                aria-controls="v-pills-home"
-                aria-selected="true"
+                onClick={() => handleTabClick("anaura")}
               >
                 Anaura
               </div>
+
+              {/* Show Anaura content under Anaura title in mobile/tablet */}
+              {isMobile && activeTab === "anaura" && (
+                <div className="fs-5 lh-base mb-4">
+                  At Anaura, we blend sustainability with style by offering premium, eco-friendly fabrics made from recycled materials.
+                  As a part of Rudra Ecovation, one of India's largest PET bottle recyclers, our mission is to replace unsustainable fabrics
+                  with beautiful, responsible alternatives. Our innovative processes create soft, breathable textiles that are both luxurious
+                  and environmentally conscious. Anaura is committed to conserving resources, reducing waste, and contributing to a sustainable future,
+                  all while providing high-quality products for the fashion industry.
+                </div>
+              )}
+
               <div
-                className="fs-4 my-3 pb-3 ps-0"
-                style={{ borderBottom: '2px dotted rgb(105, 105, 105)' }}
+                className={`fs-4 my-3 pb-3 ps-0 ${activeTab === "mission" ? 'text-primary' : ''}`}
+                style={{ borderBottom: '2px dotted rgb(105, 105, 105)', cursor: "pointer" }}
                 id="v-pills-profile-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-profile"
-                type="div"
-                role="tab"
-                aria-controls="v-pills-profile"
-                aria-selected="false"
+                onClick={() => handleTabClick("mission")}
               >
                 Mission
               </div>
+
+              {/* Show Mission content under Mission title in mobile/tablet */}
+              {isMobile && activeTab === "mission" && (
+                <div className="fs-5 lh-base mb-4">
+                  Anaura is committed to promoting sustainability through innovative textile solutions by recycling PET bottles into high-quality fabrics.
+                  Their mission is to reduce environmental impact by creating eco-friendly, stylish, and durable products that contribute to a circular economy.
+                </div>
+              )}
+
               <div
-                className="fs-4 my-3 pb-3 ps-0"
-                style={{ borderBottom: '2px dotted rgb(105, 105, 105)' }}
+                className={`fs-4 my-3 pb-3 ps-0 ${activeTab === "vision" ? 'text-primary' : ''}`}
+                style={{ borderBottom: '2px dotted rgb(105, 105, 105)', cursor: "pointer" }}
                 id="v-pills-disabled-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-disabled"
-                type="div"
-                role="tab"
-                aria-controls="v-pills-disabled"
-                aria-selected="false"
+                onClick={() => handleTabClick("vision")}
               >
                 Vision
               </div>
+
+              {/* Show Vision content under Vision title in mobile/tablet */}
+              {isMobile && activeTab === "vision" && (
+                <div className="fs-5 lh-base mb-4">
+                  Anaura envisions a world where sustainable practices are at the forefront of the fashion industry.
+                  They strive to lead by example, inspiring others to adopt eco-conscious approaches and creating a future where waste is minimized,
+                  and natural resources are preserved for generations to come.
+                </div>
+              )}
             </div>
           </div>
 
-          <div className="col-lg-8 col-md-12 mt-5 mt-lg-0">
-            <div className="tab-content" id="v-pills-tabContent">
-              <div
-                className="tab-pane fade show active"
-                id="v-pills-home"
-                role="tabpanel"
-                aria-labelledby="v-pills-home-tab"
-                tabIndex="0"
-              >
-                <h1 className="mb-4 fs-1">Recruitment for Creative People</h1>
-                <p className="fs-5 lh-base">
-                  At Anaura, we blend sustainability with style by offering
-                  premium, eco-friendly fabrics made from recycled materials. As
-                  a part of Rudra Ecovation, one of India's largest PET bottle
-                  recyclers, our mission is to replace unsustainable fabrics
-                  with beautiful, responsible alternatives. Our innovative
-                  processes create soft, breathable textiles that are both
-                  luxurious and environmentally conscious. Anaura is committed
-                  to conserving resources, reducing waste, and contributing to
-                  a sustainable future, all while providing high-quality
-                  products for the fashion industry.
-                </p>
-              </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-profile"
-                role="tabpanel"
-                aria-labelledby="v-pills-profile-tab"
-                tabIndex="0"
-              >
-                <p className="fs-5 lh-base">
-                  Anaura is committed to promoting sustainability through
-                  innovative textile solutions by recycling PET bottles into
-                  high-quality fabrics. Their mission is to reduce environmental
-                  impact by creating eco-friendly, stylish, and durable products
-                  that contribute to a circular economy.
-                </p>
-              </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-disabled"
-                role="tabpanel"
-                aria-labelledby="v-pills-disabled-tab"
-                tabIndex="0"
-              >
-                <p className="fs-5 lh-base">
-                  Anaura envisions a world where sustainable practices are at
-                  the forefront of the fashion industry. They strive to lead by
-                  example, inspiring others to adopt eco-conscious approaches
-                  and creating a future where waste is minimized, and natural
-                  resources are preserved for generations to come.
-                </p>
+          {/* For desktop/larger screens, show the content in the main right section */}
+          {!isMobile && (
+            <div className="col-lg-8 col-md-12 mt-5 mt-lg-0">
+              <div className="tab-content">
+                {/* Anaura Section */}
+                {activeTab === "anaura" && (
+                  <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                    <h1 className="mb-4 fs-1">Recruitment for Creative People</h1>
+                    <p className="fs-5 lh-base">
+                      At Anaura, we blend sustainability with style by offering premium, eco-friendly fabrics made from recycled materials.
+                      As a part of Rudra Ecovation, one of India's largest PET bottle recyclers, our mission is to replace unsustainable fabrics
+                      with beautiful, responsible alternatives. Our innovative processes create soft, breathable textiles that are both luxurious
+                      and environmentally conscious. Anaura is committed to conserving resources, reducing waste, and contributing to a sustainable future,
+                      all while providing high-quality products for the fashion industry.
+                    </p>
+                  </div>
+                )}
+
+                {/* Mission Section */}
+                {activeTab === "mission" && (
+                  <div className="tab-pane fade show active" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                    <h1 className="mb-4 fs-1">Our Mission</h1>
+                    <p className="fs-5 lh-base">
+                      Anaura is committed to promoting sustainability through innovative textile solutions by recycling PET bottles into high-quality fabrics.
+                      Their mission is to reduce environmental impact by creating eco-friendly, stylish, and durable products that contribute to a circular economy.
+                    </p>
+                  </div>
+                )}
+
+                {/* Vision Section */}
+                {activeTab === "vision" && (
+                  <div className="tab-pane fade show active" id="v-pills-disabled" role="tabpanel" aria-labelledby="v-pills-disabled-tab">
+                    <h1 className="mb-4 fs-1">Our Vision</h1>
+                    <p className="fs-5 lh-base">
+                      Anaura envisions a world where sustainable practices are at the forefront of the fashion industry.
+                      They strive to lead by example, inspiring others to adopt eco-conscious approaches and creating a future where waste is minimized,
+                      and natural resources are preserved for generations to come.
+                    </p>
+                  </div>
+                )}
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </section>
