9d89868e18bfbaeb7c60aefe6f58d40fd9308ae4
Sambit  Pattanaik <pattanaiksambit80@gmail.com>
2024-09-16 20:38:08 +0530

    ready for testing

--- src/Pages/anauraHome/homeFashionCard.jsx
+++ src/Pages/anauraHome/homeFashionCard.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import collection1 from '../../images/anaura-home/furniture-collection-1.jpg'
-import collection2 from '../../images/anaura-home/furniture-collection-2.jpg'
-import collection3 from '../../images/anaura-home/furniture-collection-3.jpg'
-import './homeFashionCard.css'
+import collection1 from '../../images/anaura-home/furniture-collection-1.jpg';
+import collection2 from '../../images/anaura-home/furniture-collection-2.jpg';
+import collection3 from '../../images/anaura-home/furniture-collection-3.jpg';
+import collection4 from '../../images/anaura-home/furniture-collection-4.jpg';
+import collection5 from '../../images/anaura-home/furniture-collection-5.jpg';
+import './homeFashionCard.css';
 
 const HomeFashionCard = () => {
   const outfits = [
     { src: collection1, alt: "collection1" },
     { src: collection2, alt: "collection2" },
     { src: collection3, alt: "collection3" },
+    { src: collection4, alt: "collection4" },
+    { src: collection5, alt: "collection5" },
   ];
 
   return (
-    <div className="d-flex justify-content-start justify-content-lg-center overflow-auto">
-      <div className="d-flex gap-4">
+    <div className="scroll-container d-flex justify-content-start justify-content-md-center">
+      <div className="card-container">
         {outfits.map((outfit, index) => (
           <div key={index} className="card" style={{ width: "18rem" }}>
             <img src={outfit.src} alt={outfit.alt} className="card-img-top rounded" />
